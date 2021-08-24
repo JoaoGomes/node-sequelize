b21b2fa0fe998123d6ee0acecb9204250942c2fd
João Eduardo Gomes <JoaoGomes@github.com>
2021-08-24 13:31:58 -0300

    Adição de comandos CRUD ao Controller.

--- routes.js
+++ routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const clients = require("./src/controllers/ClientController");
+
 const routes = express.Router();
+
+routes.get("/clients", clients.findAll);
 
 routes.get("/", (req, res) => {
 	return res.send({ name: "Ciclano Fulano" });
---
+++ src/controllers/ClientController.js
@@ -0,0 +1,52 @@
+const { SHOWINDEXES } = require("sequelize/types/lib/query-types");
+const Client = require("../models/Client");
+
+module.exports = {
+    async findAll(req, res) {
+        try {
+            const clients = await Client.findAll();
+            return res.json(clients);
+        } catch (err) {
+            return res.status(400).json( { error: err.message });
+        }
+    },
+
+    async show(req, res) {
+        try{ 
+            const client = await Client.findByPk(req.params.id);
+            return res.json(client);
+        } catch (err) {
+            return res.status(400).json( { error: err.message });
+        }
+    },
+
+    async store(req, res) {
+        try{ 
+            const client = await Client.create(req.body);
+            return res.json(client);
+        } catch (err) {
+            return res.status(400).json( { error: err.message });
+        }
+    },
+
+    async update(req, res) {
+        try{ 
+            const client = await Client.findByPk(req.params.id);
+            await client.update(req.body);
+            return res.json(client);
+        } catch (err) {
+            return res.status(400).json( { error: err.message });
+        }
+    },
+
+    async destroy(req, res) {
+        try{ 
+            const client = await Client.findByPk(req.params.id);
+            await client.destroy();
+            return res.json();
+        } catch (err) {
+            return res.status(400).json( { error: err.message });
+        }
+    },
+
+}
