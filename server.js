f7e4f76d766019558555826498581a5b36ac5e87
João Eduardo Gomes <JoaoGomes@github.com>
2021-08-24 13:00:21 -0300

    Acréscimo dos arquivos server.js e routes.js.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 
 const app = express();
 app.use(express.json());
-app.use(express.urlenconded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 app.listen(3000, () => console.log("Servidor iniciado na porta 3000"));
